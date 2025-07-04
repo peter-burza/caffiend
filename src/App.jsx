bc3457a7459323a399482cc1620db3990e2bc3f4
Peter Burza <peterburza48@gmail.com>
2025-07-04 09:08:42 +0000

    edited Header, Hooter, Layout, and CoffeeForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import CoffeeForm from './components/CoffeeForm.jsx'
+import Hero from './components/Hero.jsx'
 import Layout from './components/Layout.jsx'
+import Stats from './components/Stats.jsx'
 
 function App() {
 
 const isAuthenticated = false
 
+const authenticatedContent = (
+  <>
+    <Stats />
+    <History />
+  </>
+)
+
   return (
     <Layout>
-
+      <Hero />
+      <CoffeeForm />
+      {isAuthenticated && (authenticatedContent)}
     </Layout>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,20 @@ export default function Layout(props) {
     const { children } = props
     const header = (
         <header>
-
+            <div>
+                <h1 className="text-gradient">CAFFIEND</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
         </header>
     )
 
     const footer = (
         <footer>
-
+            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://peterburzaportfolio.netlify.app/">Peter Burza</a></p><br />
         </footer>
     )
 
