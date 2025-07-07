c4b6610863a7935f1c5bf951bcb272fa9eebebee
Peter Burza <peterburza48@gmail.com>
2025-07-07 08:10:05 +0000

    edit Stats.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import CoffeeForm from './components/CoffeeForm.jsx'
 import Hero from './components/Hero.jsx'
 import Layout from './components/Layout.jsx'
 import Stats from './components/Stats.jsx'
+import History from './components/History.jsx'
 
 function App() {
 
-const isAuthenticated = false
+const isAuthenticated = true
 
 const authenticatedContent = (
   <>
