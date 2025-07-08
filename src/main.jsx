4b5984f368fc58855e194bac895d0255dab9de0e
Peter Burza <peterburza48@gmail.com>
2025-07-08 09:33:19 +0200

    remove StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,9 +6,7 @@ import './fanta.css'
 import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>,
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 )
